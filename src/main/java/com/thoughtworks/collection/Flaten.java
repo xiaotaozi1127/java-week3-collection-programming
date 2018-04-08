85a348d064aa4af48eab82666d453d25532e7189
htao <htao@thoughtworks.com>
2018-04-08 08:57:21 +0800

    [taohui] implement flatten operator test

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,10 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
@@ -12,10 +15,17 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer[]> list = Arrays.asList(this.array);
+        List<Integer> result = new ArrayList<>();
+        list.stream().forEach(i -> {
+            List<Integer> item = Arrays.asList(i);
+            result.addAll(item);
+        });
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> flatten = this.transformToOneDimesional();
+        return flatten.stream().distinct().collect(Collectors.toList());
     }
 }
